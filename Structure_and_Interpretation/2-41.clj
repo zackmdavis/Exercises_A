0eb9b78ed32459c3a6726b94e9895732057e0195
Zack M. Davis <code@zackmdavis.net>
2013-11-29 23:48:30 -0800

    Structure and Interpretation of Computer Programs

--- Structure_and_Interpretation/2-41.clj
+++ Structure_and_Interpretation/2-41.clj
@@ -1,21 +1,11 @@
-; TODO; understanding in progress
+(defn unique-pairs [n]
+  (for [i (range n) j (range (+ i 1) n)] (list i j)))
 
-;(defn distinct-triple-sum [n s]
+(defn unique-triples [n]
+  (for [i (range n) j (range (+ i 1) n) k (range (+ j 1) n)] (list i j k)))
 
-(map (fn [i]
-       ; map to (i j) for j in range(i+1,n)
-       (range n)
+(defn distinct-triple-sum [n s]
+  (filter (fn [triple] (= (reduce + triple) s)) (unique-triples n)))
 
-   (fn [j]
-     ; map to (i j k) for k in range(j+1, n)
-     (
-
-  ;; (map (fn [i]
-  ;;        (map (fn [j]
-  ;;               (map (fn [k]
-  ;;                      (list i j k))
-  ;;                    (range j n))
-  ;;               (range i n))
-  ;;               (list i j))
-  ;;             (range i n)))
-  ;;      (range n))
+; check
+(println (distinct-triple-sum 10 12))
