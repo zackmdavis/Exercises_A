a89eb33bbbd67ce88c99cb5e1b7c9a74fd2d4857
Zack M. Davis <code@zackmdavis.net>
2014-11-11 22:25:22 -0800

    Programming Challenges
    I just can't get over how bad Racket's excuse for hash table literals
    is. If most Lisps are like this, Clojure pays for itself with just the
    map and vector literals alone.

--- Programming_Challenges/1-6-8.rkt
+++ Programming_Challenges/1-6-8.rkt
@@ -6,16 +6,54 @@
       (hash-update! counter item (lambda (x) (+ x 1)))
       counter)))
 
+(define (total-tallied tallied)
+  (for/fold ([the-total 0]) ([(_ tallies) tallied])
+    (+ the-total tallies)))
 
-(define (election candidates votes)
-  ;; TODO
-  '())
+(define (sorted-tallied tallied)
+  (sort (for/fold ([container '()]) ([(key tallies) tallied])
+          (cons (list key tallies) container))
+        (lambda (x y) (apply > (map second (list x y))))))
+
+(define (argmajority turnout standings)
+  (if (> (/ (cdr (first standings)) turnout) 0.5)
+      (car (first standings))
+      #f))
+
+(define (election candidates ballots)
+  (let* ([picks (map first ballots)]
+         [totals (tally picks)]
+         [turnout (total-tallied totals)]
+         [standings (sorted-tallied totals)]
+         [winner (argmajority turnout standings)])
+
+    ;; TODO
+
+    ;; DEBUG
+    (for ([value (list picks totals turnout standings winner)])
+      (displayln value))
+  '()))
 
 
 (require rackunit)
 
+;;; PREREQUISITES FOR DEMOCRACY
+
+;; we can count
 (check-equal? (tally '("a" "b" "b"))
               (hash-copy #hash(("a" . 1) ("b" . 2))))
+(check-equal? (total-tallied (tally '("|" "|" "|" "|")))
+                             4)
+
+;; we can tell which numbers are bigger
+(check-equal? (sorted-tallied #hash(("Jones" . 1) ("Kevin" . 11)
+                                    ("America" .  9) ("Jennifer" . 14)))
+              '(("Jennifer" 14) ("Kevin" 11)
+                ("America"  9) ("Jones" 1)))
+
+;; we can distinguish a majority
+(check-equal? (argmajority 3 (list (cons "red" 2) (cons "blue" 1)))
+              "red")
 
 ;; TODO
 (check-equal? (election '("John Doe" "Jane Smith" "Jane Austen")
