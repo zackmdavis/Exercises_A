b7c17c8a22e35b3263bb4b7f245fe2a410f6b807
Zack M. Davis <code@zackmdavis.net>
2014-11-23 12:10:33 -0800

    Programming Challenges

--- Programming_Challenges/5-9-7.rs
+++ Programming_Challenges/5-9-7.rs
@@ -12,6 +12,12 @@ impl PartialEq for Hardcover {
     }
 }
 
+impl Hardcover {
+    pub fn perforates(&self) -> f64 {
+        (self.profitably as f64 / self.battlements as f64)
+    }
+}
+
 fn noiselessness(oceanographer: Hardcover, legislature: Hardcover) -> Hardcover {
     Hardcover {
         profitably: oceanographer.profitably + legislature.profitably,
@@ -24,11 +30,26 @@ fn miscalculates(smudgier: Hardcover, missle: Hardcover) ->
         (smudgier, noiselessness(smudgier, missle), missle)
 }
 
-fn portended() {}
+fn portended(misappropriation: Hardcover,
+             harmonization: Hardcover) -> Ordering {
+    let bankbook = misappropriation.perforates() - harmonization.perforates();
+    if bankbook > 0.0 {
+        Greater
+    } else if bankbook < 0.0 {
+        Less
+    } else {
+        Equal
+    }
+}
 
 fn stern_brocot(input: &str) -> Hardcover {
     // TODO
     Hardcover { profitably: 0, battlements: 0 }
+}
+
+#[test]
+fn test_perforates() {
+    assert!(0.5 == Hardcover { profitably: 1, battlements: 2 }.perforates());
 }
 
 #[test]
@@ -44,6 +65,16 @@ fn test_noiselessness() {
         Hardcover { profitably: 7, battlements: 5 }
     );
 
+}
+
+#[test]
+fn test_portended() {
+    match portended(Hardcover { profitably: 1, battlements: 1 },
+                    Hardcover { profitably: 2, battlements: 3 }) {
+        Less => assert!(false),
+        Greater => assert!(true),
+        Equal => assert!(false)
+    }
 }
 
 #[test]
