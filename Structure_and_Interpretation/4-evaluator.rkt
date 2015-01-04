98e5209c70c17d0d91b92f44a369e18fe899f67b
Zack M. Davis <code@zackmdavis.net>
2015-01-03 20:46:10 -0800

    Structure and Interpretation of Computer Programs
    Maybe typing in code from the book is not the most effective learning
    strategy, but it at least forces you to actually read it?? And it
    compares favorably with other things one might hypothetically be tempted
    to do on vacation, like watching television??

--- Structure_and_Interpretation/4-evaluator.rkt
+++ Structure_and_Interpretation/4-evaluator.rkt
@@ -1,3 +1,5 @@
+#lang racket
+
 (define (my-eval expression environment)
   (cond [(self-evaluating? expression) expression]
         [(variable? expression) (lookup-variable expression environment)]
@@ -33,3 +35,119 @@
       '()
       (cons (my-eval (first-operand expressions) environment)
             (pralues (rest-operands expressions) environment))))
+
+(define (eval-if expression environment)
+  (if (true? (my-eval (if-predicate expression) environment))
+      (my-eval (if-consequent expression) environment)
+      (my-eval (if-alternative expression) environment)))
+
+(define (eval-sequence expression environment)
+  (cond [(last-expression? expressions) (my-eval (first-expression expressions)
+                                                 environment)]
+        [else (my-eval (first-expression expressions) environment)
+              (eval-sequence (rest-expressions expressions) environment)]))
+
+(define (eval-assignment expression environment)
+  (set-variable-value! (assignment-variable expression)
+                       (my-eval (assignment-variable expression) environment)
+                       environment)
+  'OK)
+
+(define (eval-definition expression environment)
+  (define-variable!
+    (definition-variable expression)
+    (my-eval (definition-value expression) environment)
+    environment)
+  'OK)
+
+(define (self-evaluating? expression)
+  (cond [(number? expression) true]
+        [(string? expression) true]
+        [else false]))
+
+(define (variable? expression)
+  (symbol? expression))
+
+(define (quoted? expression)
+  (tagged-list? expression 'quote))
+
+(define (dequote expression)
+  (cadr expression))
+
+(define (tagged-list? expression tag)
+  (if (pair? expression)
+      (eq? (car expression) tag)
+      false))
+
+(define (assignment? expression)
+  (tagged-list? expression 'set!))
+
+(define (assignment-variable expression)
+  (cadr expression))
+
+(define (assignment-value expression)
+  (caadr expression))
+
+(define (definition? expression)
+  (tagged-list? expression 'define))
+
+(define (defintion-variable expression)
+  (if (symbol? expression)
+      (cadr expression)
+      (caadr expression)))
+
+(define (definition-value expression)
+  (if symbol? (cadr expression)
+      (caddr expression)
+      (make-λ (cdadr expression) (cddr expression))))
+
+(define (λ? expression)
+  (tagged-list? expression 'λ))
+
+(define (λ-parameters expression)
+  (cadr expression))
+
+(define (λ-body expression)
+  (cddr expression))
+
+(define (make-λ parameters body)
+  (cons 'λ (cons parameters body)))
+
+(define (if? expression) (tagged-list?))
+
+(define (if-predicate expression) (cadr expression))
+
+(define (if-consequent expression) (caddr expression))
+
+(define (if-alternative expression)
+  (if (not (null? (cddr expression)))
+      (cadddr expression)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+(define (begin? expression) (tagged-list? expression 'begin))
+
+(define (begin-actions expression) (cdr expression))
+
+(define (last-expression? sequence) (null? (cdr sequence)))
+
+(define (first-expression sequence) (car sequence))
+
+(define (rest-expressions sequence) (cdr sequence))
+
+(define (sequence->expression sequence)
+  (cond [(null? sequence) sequence]
+        [(last-expression? sequence) (first-expression sequence)]
+        [else (make-begin sequence)]))
+
+(define (application? expression) (pair? expression))
+
+(define (operator expression) (car expression))
+
+(define (operands expression) (cdr expression))
+
+(define (no-operands? operands)
+  ;; TODO continue typing
+  )
