6f510309d5625ca2c870ea1ada6919505a80dc2f
Zack M. Davis <code@zackmdavis.net>
2015-03-11 22:21:28 -0700

    Modern Compiler Design
    recursive descent by means of throwing balls of mutable state around
    everywhere?

--- Modern_Compiler_Design/1_demo.py
+++ Modern_Compiler_Design/1_demo.py
@@ -1,6 +1,8 @@
+import sys
 
 def is_layout_character(ch):
     return ch in " \t\n"
+
 
 class Token:
     def __init__(self, ch):
@@ -13,26 +15,92 @@ class Token:
             self.representation
         )
 
+
 def lex(filename):
     with open(filename) as source_file:
         source = source_file.read()
     return (Token(ch) for ch in source if not is_layout_character(ch))
 
+
 class Expression:
+    # # actual fields monkey-patched on instance after initialization
+    # # kind = None
+    # # value = None
+    # # operator = None
+    # # left = None
+    # # right = None
+
+    def __str__(self):
+        # digit case
+        if getattr(self, 'value'):
+            print self.value
+            return
+        else:
+            for attr in ('left', 'operator', 'right'):
+                print getattr(self, attr, "__ none __")
+
+
+class DemoParsingException(Exception):
     pass
 
-class DigitExpression(Expression):
-    type = "digit"
 
-    def __init__(self, value):
-        self.value = value
+def parse_operator(under_construction, tokenstream):
+    t = next(tokenstream)
+    if t.classification == '+':
+        under_construction.operator = '+'
+        return True
+    elif t.classification == '*':
+        under_construction.operator = '*'
+        return True
+    else:
+        return False
 
-class ParenthesizedExpression(Expression):
-    type = "parenthesized"
 
-    def __init__(self, operator, left, right):
-        self.operator = operator
-        self.left = left
-        self.right = right
+def parse_expression(under_construction, tokenstream):
+    t = next(tokenstream)
 
-# to be continued ...
+    if t.classification == "digit":
+        under_construction.kind = "digit"
+        under_construction.value = int(t.representation)
+        return True
+
+    if t.classification == '(':
+        under_construction.kind = "parenthesized"
+
+        under_construction.left = Expression()
+        if not parse_expression(under_construction.left, tokenstream):
+            raise DemoParsingException("missing expression")
+
+        under_construction.operator = None
+        if not parse_operator(under_construction, tokenstream):
+            raise DemoParsingException("missing operator")
+
+        under_construction.right = Expression()
+        if not parse_expression(under_construction.right, tokenstream):
+            raise DemoParsingException("missing expression")
+
+        return True
+
+    return False
+
+
+def parse_program(filename):
+    ast_root = Expression()
+    parse_expression(ast_root, lex(filename))
+
+
+if __name__ == "__main__":
+    parse_program(sys.argv[1])
+
+# XXX doesn't work as written
+
+# zmd@ExpectedReturn:~/Code/Textbook_Exercises_A/Modern_Compiler_Design$
+# python 1_demo.py demo.demo
+# Traceback (most recent call last):
+#   File "1_demo.py", line 93, in <module>
+#     parse_program(sys.argv[1])
+#   File "1_demo.py", line 89, in parse_program
+#     parse_expression(ast_root, lex(filename))
+#   File "1_demo.py", line 76, in parse_expression
+#     raise DemoParsingException("missing operator")
+# __main__.DemoParsingException: missing operator
