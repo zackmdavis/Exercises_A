33490ba768662ee9dce92730b70c509b3919d9ed
Zack M. Davis <code@zackmdavis.net>
2015-03-30 22:02:26 -0700

    /r/dailyprogrammer

--- r_dailyprogrammer/easy_0121.rs
+++ r_dailyprogrammer/easy_0121.rs
@@ -4,8 +4,25 @@ fn exchange(k: usize) -> [usize; 3] {
     return [k/2, k/3, k/4];
 }
 
-fn exchange_feeder(real_coins: Vec<usize>, pennies: usize) {
+fn group_by() {
     // TODO
+}
+
+fn exchange_feeder(real_coins: &mut Vec<usize>, pennies: usize) -> usize {
+    if real_coins.len() == 0 {
+        pennies
+    } else {
+        let feed: usize = match real_coins.pop() {
+            Some(coin) => coin,
+            None => panic!("len() wasn't 0 so this can't actually happen; \
+                           if you thought the compiler was smart enough \
+                           to notice and let you get away with not \
+                           destructuring the Option, then you were \
+                           apparently mistaken")
+        };
+        let change: [usize; 3] = exchange(feed);
+        0 // TODO
+    }
 }
 
 #[test]
