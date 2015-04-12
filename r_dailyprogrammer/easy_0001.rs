cc5b1e523bca479fece0bde392fc6b0ccb80ada7
Zack M. Davis <code@zackmdavis.net>
2015-04-11 18:32:17 -0700

    /r/dailyprogrammer
    today I am feeling optimistic about my destiny

--- r_dailyprogrammer/easy_0001.rs
+++ r_dailyprogrammer/easy_0001.rs
@@ -6,9 +6,7 @@
 // actually know how offhand how to do child's input/output in Rust,
 // and what you don't know can't be degrading to do.
 
-// XXX: old_io is probably facing deprecation if names mean anything
-#![feature(old_io)]
-use std::old_io;
+use std::io;
 use std::collections::HashMap;
 
 fn main() {
@@ -16,34 +14,14 @@ fn main() {
     let mut collected_information = HashMap::new();
     for askable in things_to_ask_about.iter() {
         println!("What is your {}?", askable);
-        let input = old_io::stdin()
-            .read_line()
+        let mut input_buffer = String::new();
+        io::stdin()
+            .read_line(&mut input_buffer)
             .ok().expect("failure message here");
-        // XXX EVIDENCE OF MY IMPENDING DEATH in these moments when I
-        // want to scream with the righteous fury of a person who has
-        // been genuinely wronged, on the topic of what the fuck is wrong
-        // with this bullshit language where you can't even trim a string
-        // because "`input` does not live long enough" this and "borrowed
-        // value is only valid for the block suffix following statement 1
-        // at 21:48" that
-        //
-        // But what the fuck is wrong with this bullshit language is in
-        // the map, not the territory
-        //
-        // on the balance of available evidence, doesn't it seem more
-        // likely that the borrow checker is smarter than you, or that
-        // the persons who wrote the borrow checker are smarter than you?
-        //
-        // and if you can't even follow their work even after several
-        // scattered hours of dutifully trying to RTFM, will an
-        // increasingly competitive global Economy remain interested in
-        // keeping you alive and happy in the decades to come?
-        //
-        // I am not a person who has been genuinely wronged, just a man
-        // not smart enough to know any better
-        collected_information.insert(askable, input.trim());
+        // with thanks to Steve Klabnik and AndR
+        // (http://zackmdavis.net/blog/2015/03/xxx-ii/#comments)
+        collected_information.insert(askable, input_buffer.trim().to_string());
     }
-
     for (askable, response) in collected_information.iter() {
         println!("You claimed that your {} is {}.", askable, response);
     }
--- r_dailyprogrammer/intermediate_0189.rs
+++ r_dailyprogrammer/intermediate_0189.rs
@@ -2,101 +2,159 @@
 // /r/dailyprogrammer/comments
 // /2ms946/20141119_challenge_189_intermediate_roman_numeral/
 
-use std::cmp::Ordering;
-use std::cmp::Ordering::{Greater, Equal, Less};
+// This is my first Rust program that is not unredeemably laughably
+// terrible!!
 
-fn index_of_char_in_seven_element_array(array: [char; 7], target: char) ->
-    // it's a best practice to use a ridiculous name when you write a
-    // ridiculous function, to make sure that you don't get used to it
-    Option<usize>
-{
-    for (index, &item) in array.iter().enumerate() {
-        if item == target {
-            return Some(index)
+use std::collections::HashMap;
+use std::env;
+
+const PSEUDO_DIGITS: [char; 7] = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+const PSEUDO_PLACE_VALUES: [usize; 7] = [1000, 500, 100, 50, 10, 5, 1];
+
+#[allow(unused_parens)]
+fn roman_to_integer(roman: String) -> usize {
+    let mut figure_to_value: HashMap<char, usize> = HashMap::new();
+    for (&figure, &value) in PSEUDO_DIGITS.iter().zip(PSEUDO_PLACE_VALUES.iter())
+    {
+        figure_to_value.insert(figure, value);
+    }
+    let mut total_value_so_far: usize = 0;
+
+    let mut value_stack: Vec<usize> = Vec::new();
+    for figure in roman.chars() {
+        let value: usize = *(figure_to_value.get(&figure).unwrap());
+        let previously: Option<usize> = value_stack.pop();
+        // this may not be entirely idiomatic and I may not have truly
+        // seen the light of pattern-matching as yet
+        if previously.is_none() {
+            value_stack.push(value)
+        } else {
+            let previous_value: usize = previously.unwrap();
+            if previous_value >= value {
+                total_value_so_far += previous_value;
+                value_stack.push(value);
+            } else if previous_value < value {
+                total_value_so_far += (value - previous_value);
+            }
         }
     }
-    return None;
-}
+    let remaining_value = value_stack.pop();
 
-fn roman_pseudo_digit_comparator(
-    first_pseudo_digit: char, second_pseudo_digit: char
-        ) -> Ordering {
-    let pseudo_digits: [char; 7] = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-    let a = index_of_char_in_seven_element_array(pseudo_digits,
-                                                 first_pseudo_digit);
-    let b = index_of_char_in_seven_element_array(pseudo_digits,
-                                                 second_pseudo_digit);
-    if a > b {
-        Greater
-    } else if a == b {
-        Equal
-    } else if a < b {
-        Less
-    } else {
-        panic!("This can't be happening")
+    if remaining_value.is_some() {
+        total_value_so_far += remaining_value.unwrap();
     }
+
+    total_value_so_far
 }
 
-
-fn roman_to_integer(roman: String) -> usize {
-    let mut total = 0;
-    let mut previous_figure: Option<char> = None;
-    for figure in roman.chars() {
-        // TODO
-    }
-    // TODO
-    10usize
-}
-
+#[allow(unused_parens)]
 fn integer_to_roman(integer: usize) -> String {
-    let pseudo_digits: [char; 7] = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-    let pseudo_place_values: [usize; 7] = [1000, 500, 100, 50, 10, 5, 1];
-
     let mut remaining = integer;
     let mut bildungsroman = String::new();
     // get it?? It sounds like _building Roman_ (numerals), but it's
-    // also part of the story about me growing up as a programmer by
-    // learning this dreadful language
+    // also part of the story about me coming into my own as a
+    // programmer by learning a grown-up language
     //
     // XXX http://tvtropes.org/pmwiki/pmwiki.php/Main/DontExplainTheJoke
-    for ((index, value), &figure) in pseudo_place_values.iter()
-        .enumerate().zip(pseudo_digits.iter())
+    for ((index, value), &figure) in PSEUDO_PLACE_VALUES.iter()
+        .enumerate().zip(PSEUDO_DIGITS.iter())
     {
         let factor = remaining / value;
         remaining = remaining % value;
-        if factor < 4 {
+
+        if figure == 'M' || factor < 4 {
             for _ in 0..factor {
                 bildungsroman.push(figure);
             }
-        } else {
-            // XXX WRONG: `'assertion failed [...] (left: `"VIV"`, right: `"IX"`)'`
-            bildungsroman.push(figure);
-            bildungsroman.push(pseudo_digits[index-1]);
+        }
+
+        // IV, IX, XL, &c.
+        let smaller_unit_index = index + 2 - (index % 2);
+        if smaller_unit_index < PSEUDO_PLACE_VALUES.len() {
+            let smaller_unit_value = PSEUDO_PLACE_VALUES[smaller_unit_index];
+            let smaller_unit_figure = PSEUDO_DIGITS[smaller_unit_index];
+
+            if value - remaining <= smaller_unit_value {
+                bildungsroman.push(smaller_unit_figure);
+                bildungsroman.push(figure);
+                remaining -= (value - smaller_unit_value);
+            }
         }
     }
     bildungsroman
 }
 
+fn main() {
+    let mut from_integer: bool = true;
+    let mut input: String = "".to_string();
+    for (index, argument) in env::args().enumerate() {
+        match index {
+            0 => { },  // we don't care what the program is named
+            1 => { input = argument; },  // we care about what you have to say
+            2 => { if argument == "--from-roman" {
+                // and how you want us to interpret it
+                from_integer = false;
+              }
+            },
+            _ => { }  // but not about anything that comes after
+        }
+    }
+    if from_integer {
+        let parsed: usize = input.parse().ok().expect("invalid input");
+        println!("{}", integer_to_roman(parsed));
+    } else {
+        println!("{}", roman_to_integer(input));
+    }
+}
+
 
 #[test]
 fn test_roman_to_integer() {
-    // TODO
     assert_eq!(1, roman_to_integer("I".to_string()));
     assert_eq!(12, roman_to_integer("XII".to_string()));
     assert_eq!(9, roman_to_integer("IX".to_string()));
     assert_eq!(98, roman_to_integer("XCVIII".to_string()));
-
+    assert_eq!(3998, roman_to_integer("MMMCMXCVIII".to_string()));
 }
 
 #[test]
-fn test_roman_to_integer_nonsubtractive_cases() {
+fn test_roman_to_integer_with_some_nonsubtractive_cases() {
     assert_eq!(integer_to_roman(1), "I".to_string());
     assert_eq!(integer_to_roman(12), "XII".to_string());
 }
 
 #[test]
-fn test_integer_to_roman_with_subtractive_cases() {
-    // TODO
+fn test_integer_to_roman_with_some_subtractive_cases() {
     assert_eq!(integer_to_roman(9), "IX".to_string());
-    assert_eq!(integer_to_roman(98), "XCVIII".to_string());
+    assert_eq!(integer_to_roman(48), "XLVIII".to_string());
+}
+
+#[test]
+fn integer_to_roman_grand_test() {
+    let seventy_six_to_one_hundred_twelve_inclusive = [
+        "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII",
+        "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII",
+        "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI",
+        "XCVII", "XCVIII", "XCIX", "C", "CI", "CII", "CIII", "CIV", "CV",
+        "CVI", "CVII", "CVIII", "CIX", "CX", "CXI", "CXII"];
+
+    for (integer, expected_roman) in (76..113).zip(
+        seventy_six_to_one_hundred_twelve_inclusive.iter())
+    {
+        assert_eq!(expected_roman.to_string(), integer_to_roman(integer));
+    }
+}
+
+#[test]
+fn integer_to_roman_stress_test() {
+    assert_eq!("".to_string(), integer_to_roman(0));
+
+    let tail_to_four_thousand = ["MMMCMXCVIII", "MMMCMXCIX", "MMMM"];
+    for (integer, expected_roman) in (3998..4001).zip(
+        tail_to_four_thousand.iter())
+    {
+        assert_eq!(expected_roman.to_string(), integer_to_roman(integer));
+    }
+
+    assert_eq!("MMMMMMXVI".to_string(), integer_to_roman(6016));
 }
