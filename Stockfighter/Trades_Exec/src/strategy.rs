1f7d0931e5cc4c35120b48e78b73f06782c02cf6
Zack M. Davis <code@zackmdavis.net>
2015-12-25 14:06:59 -0800

    Stockfighter
    This game is extremely demoralizing—I can write (ugly) code (that I
    don't feel motivated to make less ugly when it's just to hit an API for
    a game), but I can't write code that consistently makes money and I
    don't know how to fix it. I technically cleared level 3 by trying out a
    cheap hack someone mentioned in the
    forums (https://discuss.starfighters.io/t/level-3-cheap-win-bug-or-feature/3950),
    where there's some bot that makes market orders that you can exploit by
    leaving a ludicrously overpriced ask and waiting until it just happens
    to rest on the top of the book with the idiot market buy order comes
    in. That's not a real victory—I can't expect to make progress on the
    remaining levels unless I actually fix the hole in my model (and my
    code).
    
    Maybe I just need to stop being lazy—I've had this attitude that "Eh,
    this isn't supposed to be hard yet, I can just bang out this sloppy
    approximate code and expect it to pretty much beat the bot market." But
    now I've seen that the bot market is smarter than that—only after
    exerting the effort to formulate the _right_ code expressing the
    _correct_ concepts, should I feel despondent and humiliated.

--- Stockfighter/Trades_Exec/src/strategy.rs
+++ Stockfighter/Trades_Exec/src/strategy.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use std::collections::VecDeque;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -46,10 +47,22 @@ pub fn maker(fighter: Stockfighter, mut our_position: Position) {
 
     let mut market = 0;
 
+    let mut outstanding_buy_order_ids = VecDeque::new();
+    let mut outstanding_sell_order_ids = VecDeque::new();
+
     let mut top_bid = Bid::new_bid(0, 0);
     let mut top_ask = Ask::new_ask(0, 0);
 
-    // XXX this is significantly worse than chance
+    // XXX: what am I doing wrong?! I feel like you should be able to
+    // consistently make money by playing the spread (I'll sell to anyone above
+    // X or buy from anyone below Y, for an expected profit of X-Y per
+    // transaction per share) and that level 3 isn't supposed to be hard, but
+    // it just hasn't been working out for me
+    //
+    // I did end up "clearing" level 3 by testing out a stupid hack that
+    // someone mentioned in the forums
+    // (https://discuss.starfighters.io/t/level-3-cheap-win-bug-or-feature/3950),
+    // but that obviously doesn't morally count
 
     loop {
         info!("Start of the trading loop!");
@@ -68,64 +81,79 @@ pub fn maker(fighter: Stockfighter, mut our_position: Position) {
         let buy_limit = BOUND_LONG - our_position.inventory;
         let sell_limit = our_position.inventory - BOUND_SHORT;
 
-        let mut buy_order_id = None;
-        let mut sell_order_id = None;
+        if book.bids.len() > 0 {
+            top_bid = book.bids[0];
+        }
+        if book.asks.len() > 0 {
+            top_ask = book.asks[0];
+        }
+
+        market = (top_bid.price + top_ask.price) / 2;
+        let spread = top_ask.price - top_bid.price;
+        let undercut = spread * 2 / 5;
+
+        let mut buy_this_many = 0;
+        let mut sell_this_many = 0;
 
         // up to risk limits, I can sell as many shares as I remember having
         // bought for not more
         if book.bids.len() > 0 {
-            top_bid = book.bids[0];
             let net_shares_acquired_cheaper = our_position
                 .trades_in_range(None, Some(top_bid.price));
             if net_shares_acquired_cheaper >= 0 {
-                let sell_this_many = if net_shares_acquired_cheaper == 0 {
+                sell_this_many = if net_shares_acquired_cheaper == 0 {
                     cmp::min(sell_limit, top_bid.quantity)
                 } else {
                     min_i64!(sell_limit, top_bid.quantity,
                              net_shares_acquired_cheaper)
                 };
-                if sell_this_many > 0 {
-                    sell_order_id = Some(fighter.order(
-                        sell_this_many, top_bid.price, Action::Sell,
-                        OrderType::Limit
-                    ));
-                }
             }
         }
 
         // up to risk limits, I can buy as many shares as I remember having
         // sold for not less
         if book.asks.len() > 0 {
-            top_ask = book.asks[0];
             let net_shares_sold_more_expensively = our_position
                 .trades_in_range(Some(top_ask.price), None);
             if net_shares_sold_more_expensively <= 0 {
-                let buy_this_many = if net_shares_sold_more_expensively == 0 {
+                buy_this_many = if net_shares_sold_more_expensively == 0 {
                     cmp::min(buy_limit, top_ask.quantity)
                 } else {
                     min_i64!(buy_limit, top_ask.quantity,
                              -net_shares_sold_more_expensively)
                 };
-                if buy_this_many > 0 {
-                    buy_order_id = Some(fighter.order(
-                        buy_this_many, top_ask.price, Action::Buy,
-                        OrderType::Limit
-                    ));
-                }
             }
         }
 
-        market = (top_bid.price + top_ask.price) / 2;
+        let min_for_balance = cmp::min(buy_this_many, sell_this_many);
+        buy_this_many = min_for_balance;
+        sell_this_many = min_for_balance;
 
-        // How did we do??
-        if let Some(order_id) = buy_order_id {
+        if sell_this_many > 0 {
+            let sell_order_id = fighter.order(
+                sell_this_many, top_ask.price - undercut, Action::Sell,
+                OrderType::Limit
+            );
+            outstanding_sell_order_ids.push_back(sell_order_id)
+        }
+        if buy_this_many > 0 {
+            let buy_order_id = fighter.order(
+                buy_this_many, top_bid.price + undercut, Action::Buy,
+                OrderType::Limit
+            );
+            outstanding_buy_order_ids.push_back(buy_order_id)
+        }
+
+        // How did we do on old trades?? Cancel them and record the fills
+        if outstanding_buy_order_ids.len() > 2 {
+            let order_id = outstanding_buy_order_ids.pop_front().unwrap();
             let buy_fills = fighter.cancel_order(order_id);
             for buy in &buy_fills {
                 our_position.record_fill(buy);
             }
         }
-
-        if let Some(order_id) = sell_order_id {
+        if outstanding_sell_order_ids.len() > 2 {
+            let order_id = outstanding_sell_order_ids.pop_front().unwrap();
             let sell_fills = fighter.cancel_order(order_id);
             for sale in &sell_fills {
                 our_position.record_fill(sale);
