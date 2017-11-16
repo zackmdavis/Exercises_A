422187ec04994ca5f2a0fa643a727bca27d69ab4
Zack M. Davis <code@zackmdavis.net>
2017-11-15 19:53:25 -0800

    idly fixing old "Cracking" exercise code
    This was (bad, not-even-compilable) pre-1.0 Rust!

--- Cracking_the_Coding_Interview/01-07.rs
+++ Cracking_the_Coding_Interview/01-07.rs
@@ -1,43 +1,36 @@
 // "Write an algorithm such that if an element in an MxN matrix is 0,
 // its entire row and column are set to 0.
 
-// ... except I'm retarded and not used to programming lanugages where
-// you're actually expected to understand how computers work, so what
-// happens if I restrict it to "4 x 4 matrix"? Can I fucking do
-// something in that case?
+// TODO: finish
 
-fn zero_row(matrix: &mut [[int, ..4], ..4], row: uint) {
+fn zero_row(matrix: &mut [[isize; 4]; 4], row: usize) {
     let row_length = matrix[0].len();
-    for j in range(0u, row_length) {
+    for j in 0..row_length {
         matrix[row][j] = 0;
     }
 }
 
-fn zero_col(matrix: &mut [[int, ..4], ..4], col: uint) {
+fn zero_col(matrix: &mut [[isize; 4]; 4], col: usize) {
     let col_length = matrix.len();
-    for i in range(0u, col_length) {
+    for i in 0..col_length {
         matrix[i][col] = 0;
     }
 }
 
 #[test]
 fn test_zero_row_and_col() {
-    let mut my_matrix: [[int, ..4], ..4] = [[1i, 2, 3, 4],
-                                            [1, 2, 3, 4],
-                                            [1, 2, 3, 4],
-                                            [1, 2, 3, 4]];
-    // ffffff "mismatched types: expected `&mut [[int, .. 4], .. 4]`
-    // but found `&[[int, .. 4], .. 4]` (values differ in mutability)"
-    // but I _said_ `my_matrix` is mutable, and I ... need to do more
-    // reading before trying to write the simplest of programs??
-    zero_row(&my_matrix, 1);
-    zero_col(&my_matrix, 2);
-    let mut expectation = [[1i, 2, 0, 4],
-                           [0, 0, 0, 0],
-                           [1, 2, 0, 4],
-                           [1, 2, 0, 4]];
-    for i in range(0u, 3) {
-        for j in range(0u, 3) {
+    let mut my_matrix: [[isize; 4]; 4] = [[1, 2, 3, 4],
+                                          [1, 2, 3, 4],
+                                          [1, 2, 3, 4],
+                                          [1, 2, 3, 4]];
+    zero_row(&mut my_matrix, 1);
+    zero_col(&mut my_matrix, 2);
+    let expectation = [[1, 2, 0, 4],
+                       [0, 0, 0, 0],
+                       [1, 2, 0, 4],
+                       [1, 2, 0, 4]];
+    for i in 0..3 {
+        for j in 0..3 {
             assert!(expectation[i][j] == my_matrix[i][j]);
         }
     }
