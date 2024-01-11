98e360d0c2b27a24593dd8860a9d769161817ec8
Zack M. Davis <code@zackmdavis.net>
2024-01-10 20:44:22 -0800

    ARENA: GAN architecture

--- ARENA/gan_vae.py
+++ ARENA/gan_vae.py
@@ -55,7 +55,7 @@ def conv2d_minimal(x, weights):
     return einops.einsum(x_strided, weights, "b ic oh ow kh kw, oc ic kh kw -> b oc oh ow")
 
 
-def force_pair(v: IntOrPair) -> Pair:
+def force_pair(v):
     if isinstance(v, tuple):
         if len(v) != 2:
             raise ValueError(v)
@@ -70,9 +70,6 @@ class ConvTranspose2d(nn.Module):
     def __init__(
         self, in_channels, out_channels, kernel_size, stride=1, padding=0
     ):
-        '''
-        Same as torch.nn.ConvTranspose2d with bias=False.
-        '''
         super().__init__()
 
         self.in_channels = in_channels
@@ -117,93 +114,216 @@ class ConvTranspose2d(nn.Module):
         ])
 
 
-class Generator(nn.Module):
-    def __init__(
-        self,
-        latent_dim_size: int = 100, # "the size of the latent dimension, i.e. the input to the generator"
-        img_size: int = 64, # "the size of the image, i.e. the output of the generator"
-        img_channels: int = 3, # "the number of channels in the image (3 for RGB, 1 for grayscale)"
-        hidden_channels: List[int] = [128, 256, 512],
-            # "the number of channels in the hidden layers of the generator (starting closest
-            # to the middle of the DCGAN and going outward, i.e. in chronological order for
-            # the generator)"
-            #
-            # uh, I'm confused that they call `[128, 256, 512]` "chronological
-            # order" for the generator, given the diagram that shows the 128 on
-            # the side closer to the outputted image ...? But on the other
-            # hand, increasing channels from latent to output for the generator
-            # makes sense (and GPT-4 confirms this understanding under questioning).
-            #
-            # Well, at least the "closest to the middle" part makes
-            # sense. Maybe it's an erratum.
-    ):
-        n_layers = len(hidden_channels)
-        assert img_size % (2 ** n_layers) == 0, "activation size must double at each layer"
-
-        super().__init__()
-
-        # But what should `project_and_reshape` do? It's supposed to take an
-        # 100-element latent vector to a 4×4×1024? GPT-4 suggests that this is
-        # going to be a `Linear` ...
-        self.project_and_reshape = nn.Linear(latent_dim_size, ((img_size//16) ** 2) * 2 *hidden_channels[-1])
-        self.hidden_layers = nn.Sequential(
-            # Just filling in the numbers from the diagram, presumably?
-            ConvTranspose2d(1024, 512, 8, stride=2),
-            ConvTranspose2d(512, 256, 16, stride=2),
-            ConvTranspose2d(256, 128, stride=2),
-            ConvTranspose2d(128, 64, stride=2),
-        )
-
-        return
-
-    def forward(self, x):
-        x = self.project_and_reshape(x)
-        x = self.hidden_layers(x)
-        return x
-
 
 class Discriminator(nn.Module):
     def __init__(
         self,
-        img_size: int = 64,
-        img_channels: int = 3,
-        hidden_channels: List[int] = [128, 256, 512],
+        img_size=64,
+        img_channels=3,
+        hidden_channels=[128, 256, 512, 1024],
     ):
-        '''
-        Implements the discriminator architecture from the DCGAN paper (the mirror image of
-        the diagram at the top of page 4). We assume the size of the activations doubles at
-        each layer (so image size has to be divisible by 2 ** len(hidden_channels)).
-
-        Args:
-            img_size:
-                the size of the image, i.e. the input of the discriminator
-            img_channels:
-                the number of channels in the image (3 for RGB, 1 for grayscale)
-            hidden_channels:
-                the number of channels in the hidden layers of the discriminator (starting
-                closest to the middle of the DCGAN and going outward, i.e. in reverse-
-                chronological order for the discriminator)
-        '''
         n_layers = len(hidden_channels)
         assert img_size % (2 ** n_layers) == 0, "activation size must double at each layer"
 
         super().__init__()
-        pass
 
-    def forward(self, x: t.Tensor) -> t.Tensor:
-        pass
+        channel_sequence = [img_channels] + hidden_channels
+
+        convolutional_layers = []
+        for i in range(len(channel_sequence) - 1):
+            convolutional_layers.append(
+                nn.Conv2d(channel_sequence[i], channel_sequence[i+1], kernel_size=4, stride=2, padding=1, bias=False)
+            )
+            if i != 0: # "All blocks have a batchnorm layer, except for the very first one."
+                convolutional_layers.append(nn.BatchNorm2d(channel_sequence[i+1]))
+            convolutional_layers.append(LeakyReLU(0.2))
+
+        self.convolve = nn.Sequential(*convolutional_layers)
+
+        self.predict = nn.Sequential(*[
+            # So at the end of the convolutional blocks, we have a batch×1024×4×4, which we flatten to batch×16384
+            nn.Flatten(),
+            # and then use a fully-connected layer to map it to batch×1
+            nn.Linear(channel_sequence[-1] * (img_size//(2 ** n_layers))**2, 1, bias=False),
+            # and then sigmoid it to a probability.
+            Sigmoid(),
+        ])
+
+    def forward(self, x):
+        return self.predict(self.convolve(x))
 
 
-class DCGAN(nn.Module):
-    netD: Discriminator
-    netG: Generator
-
+class Generator(nn.Module):
     def __init__(
         self,
-        latent_dim_size: int = 100,
-        img_size: int = 64,
-        img_channels: int = 3,
-        hidden_channels: List[int] = [128, 256, 512],
+        latent_dim_size=100,
+        img_size=64,
+        img_channels=3,
+        hidden_channels=[128, 256, 512, 1024],
+    ):
+        n_layers = len(hidden_channels)
+        assert img_size % (2 ** n_layers) == 0, "activation size must double at each layer"
+
+        super().__init__()
+
+        channel_sequence = list(reversed(hidden_channels)) + [img_channels]
+
+        self.project_and_reshape = nn.Sequential(
+            # fully-connected layer from batch×latent_dim_size to batch×16384
+            nn.Linear(latent_dim_size, channel_sequence[0] * (img_size//(2 ** n_layers))**2, bias=False),
+            # shaped as 1024×4×4
+            nn.Unflatten(1, (channel_sequence[0], img_size//(2 ** n_layers), img_size//(2 ** n_layers))),
+            nn.BatchNorm2d(channel_sequence[0]),
+            nn.ReLU(),
+        )
+
+        transconvolutional_layers = []
+        for i in range(len(channel_sequence) - 1):
+            transconvolutional_layers.append(
+                ConvTranspose2d(channel_sequence[i], channel_sequence[i+1], kernel_size=4, stride=2, padding=1),
+            )
+            # last layer has no BatchNorm and tanh instead of ReLU
+            if i != len(channel_sequence) - 2:
+                transconvolutional_layers.extend([
+                    nn.BatchNorm2d(channel_sequence[i+1]),
+                    nn.ReLU(),
+                ])
+            else:
+                transconvolutional_layers.extend([
+                    Tanh()
+                ])
+
+        self.generate = nn.Sequential(*transconvolutional_layers)
+
+    def forward(self, x):
+        return self.generate(self.project_and_reshape(x))
+
+
+# OK, after finally reading enough to understand what's going on, it's time to
+# compare to the solutions and then train it.
+#
+# I didn't end up with the same parameter counts as the canonical solutions
+# (and I have a different `hidden_channels` default, because the diagram and
+# the explanatory text seemed to contradict each other, and I went with the
+# diagram), but at least the sequence of layers looks the same?
+
+# My gan_vae.Discriminator(hidden_channels=[128, 256, 512]) has 2,759,041
+# params; instructor's has 2,661,888.
+
+# The Conv2D/BatchNorm in the middle match, the next Conv2D is almost the
+# same—difference of 512 probably a matter of me using the torch.nn version and
+# needing to pass bias=False?. But in my first Conv2d, I have 131,200
+# vs. instructor's 6,144, and in my linear at the end, I have 4,097
+# vs. instructor's 32,768.
+
+# So what happened? I now insert the `bias=False` kwargs, and it turns out that
+# I forgot to square the final downsampled sized in the Linear at the end.
+# Then the only remaining mismatch is, why is my first Conv2d layer so big
+# (131,072 vs. expected 6,144)? Can `torchinfo` give me more details than the param count?
+
+# GPT-4 suggests `model.modules()` instead of conjectured `torchinfo`
+# functionality.  I had a `Conv2d(64, 128, kernel_size=(4, 4), stride=(2, 2),
+# padding=(1, 1), bias=False)` where the instructor's soultion had a
+# `Conv2d(in_channels=3, out_channels=128, kernel_size=4, stride=2,
+# padding=1)`—I see, because I used `img_size` instead of `img_channels` at the
+# start of my channel sequence.
+
+# Embarrassingly late lightbulb moment: a convolutional layer doesn't need to
+# "know" about big the image is, does it? Because it's only learning weights
+# for the kernel which gets slid over the image?
+
+# OK, now the discriminators match! What about the generators? The only
+# difference seems to be that I have many fewer parameters in my first linear
+# layer. Mine is a `Linear(in_features=100, out_features=4096, bias=False)`;
+# instructor's is a `Linear(in_features=100, out_features=32768,
+# bias=False)`—because I forgot to square again.
+
+
+# Now it's time to train. We get a zipfile with a lot of celebrity faces, and
+# code to load either that or MNIST digits.
+
+# The face images are taking a long time to unzip! It took about twenty minutes
+# to get halfway!
+#
+# zmd@system76-pc:~/Code/Exercises_A/ARENA$ unzip -l img_align_celeba.zip  | wc -l
+# 202605
+# zmd@system76-pc:~/Code/Exercises_A/ARENA$ ll data/celeba/img_align_celeba/ -1 | wc -l
+# 104406
+
+import pathlib
+
+from torchvision import datasets, transforms
+
+def get_dataset(dataset, train):
+    assert dataset in ["MNIST", "CELEB"]
+
+    if dataset == "CELEB":
+        image_size = 64
+        assert train, "CelebA dataset only has a training set"
+        transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.CenterCrop(image_size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+        trainset = datasets.ImageFolder(
+            root = pathlib.Path.cwd() / "data" / "celeba",
+            transform = transform
+        )
+
+    elif dataset == "MNIST":
+        img_size = 28
+        transform = transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        trainset = datasets.MNIST(
+            root = pathlib.Path.cwd() / "data",
+            transform = transform,
+            download = True,
+        )
+
+    return trainset
+
+
+# Well, there was also a weight-initialization exercise. Let's do that on our DCGAN wrapper class.
+
+class DCGAN(nn.Module):
+    def __init__(
+        self,
+        latent_dim_size=100,
+        img_size=64,
+        img_channels=3,
+        hidden_channels=[128, 256, 512],
     ):
         super().__init__()
-        pass
+        self.latent_dim_size = latent_dim_size
+        self.img_size = img_size
+        self.img_channels = img_channels
+        self.hidden_channels = hidden_channels
+        self.discriminator = Discriminator(img_size, img_channels, hidden_channels)
+        self.generator = Generator(latent_dim_size, img_size, img_channels, hidden_channels)
+
+    def initialize_weights(self):
+        for network in [self.discriminator, self.generator]:
+            for module in network.modules():
+                # This is gross (maybe I should have been more careful about
+                # meaningful layer property names in my network classes), but
+                # it'll do
+                if module.layer.__class__.__name__ == "BatchNorm2d":
+                    nn.init.normal_(module.weight, 1, 0.02)
+                    nn.init.constant_(module.bias, 0)
+                if module.layer.__class__.__name__ in ["Conv2d", "ConvTranspose2d", "Linear"]:
+                    nn.init.normal_(module.weight, 0, 0.02)
+        # OK, the instructor's solution used `isinstance` instead of
+        # `.__class__.__name__`, which is more natural and I don't know why I
+        # didn't think it
+        #
+        # (I had also missed that `Linear` gets the same inits as the trans/cis
+        # convolutional layers)
+
+
+# (I also set the slope on my discriminator LeakyReLUs to 0.2 to match the solution.)
+
+# Now to write the training loop! ... tomorrow?
